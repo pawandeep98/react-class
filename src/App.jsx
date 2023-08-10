5e1cc32843c767c2fb4c9edada6b542d4a6f61e3
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-10 22:36:46 +0530

    car ko cardholder kr dia

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { } from 'react'
 import HeaderComponent from './HeaderComponent.jsx'
 import Search from './Search.jsx'
 import Greenband from './Greenband.jsx'
-import Card from './Card.jsx'
+import CardHolder from './CardHolder.jsx'
 
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-   <HeaderComponent/>
-    <Greenband/>
-    <Search/>
-    <Card/>
+      <HeaderComponent />
+      <Greenband />
+      <Search />
+      <CardHolder />
     </>
   )
 }
